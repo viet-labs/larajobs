d037d5cc3216cf2eeac7af2e854cbac2eef485fa
Nguyen Cong Viet <vietlp1000@gmail.com>
2024-07-10 22:50:01 +0700

    feat: add files to forder migrations

---
+++ database/migrations/2024_07_09_144525_create_tu_khoa_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tu_khoa', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('ten_hien_thi');
+            $table->string('slug');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tu_khoa');
+    }
+};
